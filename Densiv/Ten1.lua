548106fe58db146b4b36592c2eda91563825d6b1
oRHDoshacks <ohackrh@gmail.com>
2024-08-01 16:54:01 -0300

    creat ten1

---
+++ Densiv/Ten1.lua
@@ -0,0 +1,18 @@
+--Comentarios!
+print("Hello My Word")
+print("Hello!\nMy\nWord")
+
+a="Hello"
+b="My"
+c="Word"
+print(a.." "..b.." "..c)
+
+--[[
+Comentarios
+de multiplas
+linhas
+]]
+a=1
+b="um"
+c="1"
+
